c9b499a77c3fb6b43c4bad12572cc4c88794d6c6
Giacomo Vacca <giacomo.vacca@gmail.com>
2022-12-21 17:29:06 +0100

    reattach: Add failure case with bad auth_state

--- internal/e2e-js/utils.ts
+++ internal/e2e-js/utils.ts
@@ -86,6 +86,9 @@ export const createTestRoomSession = async (
       })
 
       // @ts-expect-error
+      window.jwt_token = options.API_TOKEN
+
+      // @ts-expect-error
       window._roomObj = roomSession
 
       return Promise.resolve(roomSession)
@@ -93,6 +96,59 @@ export const createTestRoomSession = async (
     {
       RELAY_HOST: process.env.RELAY_HOST,
       API_TOKEN: vrt,
+      initialEvents: options.initialEvents,
+      roomSessionOptions: options.roomSessionOptions,
+    }
+  )
+}
+
+
+export const createTestRoomSessionWithJWT = async (
+  page: Page,
+  options: {
+    vrt: CreateTestVRTOptions
+    /** set of events to automatically subscribe before room.join() */
+    initialEvents?: string[]
+    roomSessionOptions?: Record<string, any>
+  },
+  jwt: string
+) => {
+  if (!jwt) {
+    console.error('Invalid JWT. Exiting..')
+    process.exit(4)
+  }
+  return page.evaluate(
+    (options) => {
+      // @ts-expect-error
+      const Video = window._SWJS.Video
+      const roomSession = new Video.RoomSession({
+        host: options.RELAY_HOST,
+        token: options.API_TOKEN,
+        rootElement: document.getElementById('rootElement'),
+        audio: true,
+        video: true,
+        logLevel: 'warn',
+        // debug: {
+        //   logWsTraffic: true,
+        // },
+        ...options.roomSessionOptions,
+      })
+
+      options.initialEvents?.forEach((event) => {
+        roomSession.once(event, () => {})
+      })
+
+      // @ts-expect-error
+      window.jwt_token = options.API_TOKEN
+
+      // @ts-expect-error
+      window._roomObj = roomSession
+
+      return Promise.resolve(roomSession)
+    },
+    {
+      RELAY_HOST: process.env.RELAY_HOST,
+      API_TOKEN: jwt,
       initialEvents: options.initialEvents,
       roomSessionOptions: options.roomSessionOptions,
     }
